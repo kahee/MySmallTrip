b169b79aae4f753d5da623c1fe9b1a3ec9a652ae
kahee <yuygh131@gmail.com>
2018-04-02 11:06:38 +0900

    API URL 기본 회원가입 가능

--- app/members/serializer.py
+++ app/members/serializer.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
@@ -15,14 +16,26 @@ class UserSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
+    # def validate(self, attrs):
+    #     password = attrs.get('password')
+    #     password2 = attrs.get('password2')
+    #
+    #     print(password,password2)
+    #
+    #     if password != password2:
+    #         raise ValidationError("비밀번호가 일치하지 않습니다.")
+    #
+    #     return attrs
+
     def create(self, validate_data):
+        password = validate_data['password']
 
         user = User.objects.create_user(
             email=validate_data['email'],
-            password=validate_data['password'],
             username=validate_data['email'],
             first_name=validate_data['first_name'],
         )
+        user.set_password(password)
         return user
 
     class Meta:
