ecf960e32151e475c229de5516b52947e33c4118
kahee <yuygh131@gmail.com>
2018-04-03 16:00:41 +0900

    apis, serializer 패키지화

--- app/members/serializer/user_serializer.py
+++ app/members/serializer/user_serializer.py
@@ -1,13 +1,8 @@
-import os
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
-
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
