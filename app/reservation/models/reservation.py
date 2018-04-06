2e0a034eb13a2de944f81fba0185a85f27c3feb2
kahee <yuygh131@gmail.com>
2018-04-06 15:35:02 +0900

    User 모델 사용할 때 get_user_model() 사용, 충돌 수정

--- app/reservation/models/reservation.py
+++ app/reservation/models/reservation.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
+
 from members.models import *
 from reservation.models.reservation_base import ReservationBase
 from travel.models import TravelInformation, TravelSchedule
 
+User = get_user_model()
 
 class Reservation(ReservationBase):
     travel_Schedule = models.ForeignKey(
