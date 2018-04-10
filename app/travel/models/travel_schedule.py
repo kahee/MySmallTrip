81093a40e59a29c8ed4dec086e5999eaccc39e31
seokjae hong <hsj2334@gmail.com>
2018-04-10 12:34:27 +0900

    city 모델 수정, schedule 모델 enddate null추가, 마이그레이션

--- app/travel/models/travel_schedule.py
+++ app/travel/models/travel_schedule.py
@@ -10,7 +10,7 @@ class TravelSchedule(ProductBase):
         on_delete=models.CASCADE,
         verbose_name='travel_info')
     start_date = models.DateField('여행시작날짜')
-    end_date = models.DateField('여행끝날짜', blank=True)
+    end_date = models.DateField('여행끝날짜', blank=True,null=True)
 
 
     class Meta:
