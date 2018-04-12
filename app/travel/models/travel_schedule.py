c644d8041a37addba7ec44e4f1759fd8487155c0
seokjae hong <hsj2334@gmail.com>
2018-04-12 10:31:03 +0900

    충돌병합

--- app/travel/models/travel_schedule.py
+++ app/travel/models/travel_schedule.py
@@ -20,14 +20,6 @@ class TravelSchedule(ProductBase):
         verbose_name='travel_info')
     travelschedule_user = models.ManyToManyField(User, through='reservation.Reservation')
 
-
-    reserved_user = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        through='reservation.Reservation',
-        related_name="reserve_travel",
-        blank=True,
-    )
-
     reserved_people = models.IntegerField(default=0)
     start_date = models.DateField('여행시작날짜')
     end_date = models.DateField('여행끝날짜', blank=True, null=True)
