abcd5e2275afdad0e5e521db079ba27961a99d77
kahee <yuygh131@gmail.com>
2018-04-23 15:23:46 +0900

    UserChangePasswordView 생성 및 url 추가

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .apis.user_detail import UserDetailView, UserDetailImageView, UserDetailPhoneNumberView
+from .apis.user_detail import UserDetailView, UserChangeImageView, UserChangePhoneNumberView, UserChangePasswordView
 
 urlpatterns = [
     path('info/', UserDetailView.as_view(), name='user-detail'),
-    path('info/img-change/', UserDetailImageView.as_view(), name='user-image-change'),
-    path('info/phone-change/', UserDetailPhoneNumberView.as_view(), name='user-phonenumber-change'),
-    # path('reservation/', UserReservationListView.as_view(), name='user-reservation-list'),
-    # path('reservation/cancel/', UserReservationCancelListView.as_view(), name='user-reservation-cancel-list'),
-
+    path('info/password/', UserChangePasswordView.as_view(), name='user-password'),
+    path('info/img-change/', UserChangeImageView.as_view(), name='user-image-change'),
+    path('info/phone-change/', UserChangePhoneNumberView.as_view(), name='user-phonenumber-change'),
 ]
